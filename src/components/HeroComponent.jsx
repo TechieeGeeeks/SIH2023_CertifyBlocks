afde87a586c7b0c7557e10cadf1160d530abb464
vwakesahu <89719144+vwakesahu@users.noreply.github.com>
2023-09-21 11:54:36 +0530

    UI changes for mobile screen

--- src/components/HeroComponent.jsx
+++ src/components/HeroComponent.jsx
@@ -3,7 +3,7 @@ import HeroImg from "../img/hero_img.png";
 const HeroComponent = () => {
   return (
     <section
-      className=" h-screen grid grid-cols-1 md:grid-cols-2 w-full "
+      className="md:h-screen grid grid-cols-1 md:grid-cols-2 w-full "
       id="home"
     >
       <div className="md:w-[80%] flex-1 flex flex-col md:items-start items-center justify-center md:h-[75%] gap-6">
@@ -14,7 +14,7 @@ const HeroComponent = () => {
         <p className="text-justify text-white font-bold text-sm flex flex-wrap gap-1">
         Currently large no of training programs is organized, and certificates are provided. There is no mechanism to validate digital certificate.so create a system in which custom digital certificate generate. User can store certificate in digital locker system other organization will validate certificate. Use opensource software and blockchain technology. Expected Output: Blockchain Based Certificate generation and validation Certificate can be added in Digital Loker System Users: Government Office, Student, Industry, Institutes
         </p>
-        <div>
+        <div className="mt-5">
           <a className="mt-10 rounded-lg bg-white py-3 px-9 text-lg font-semibold text-bgColor hover:shadow-lg duration-100 transition-all ease-in-out cursor-pointer" target="_blank" href="https://sih.gov.in/sih2023PS">
             Official Site
           </a>
